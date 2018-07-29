cacaedeec49f5e15cfb6becdc811571c20fa6de0
karnakar reddy thoorpu <thoorpukarnakar@gmail.com>
2018-07-29 15:52:45 +0530

    Add files via upload

---
+++ DeepKarNets.py
@@ -0,0 +1,140 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+# In[5]:
+
+
+mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
+
+
+# In[17]:
+
+
+sess = tf.InteractiveSession()
+
+
+# In[18]:
+
+
+x = tf.placeholder(tf.float32, shape=[None,784])
+y_ = tf.placeholder(tf.float32,[None,10])
+
+
+# In[19]:
+
+
+x_image = tf.reshape(x,[-1,28,28,1],name = "x_image")
+
+
+# In[20]:
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape,stddev = 0.1)
+    return tf.Variable(initial)
+
+
+# In[21]:
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1,shape=shape)
+    return tf.Variable(initial)
+
+
+# In[22]:
+
+
+def conv2d(x,W):
+    return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding = 'SAME')
+
+
+# In[23]:
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x,ksize=[1,2,2,1], strides=[1,2,2,1],padding='SAME')
+
+
+# In[24]:
+
+
+#first convolutional layer
+W_conv1 = weight_variable([5,5,1,32]) 
+b_conv1 = bias_variable([32])
+h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+
+# In[25]:
+
+
+W_conv2 = weight_variable([5,5,32,64]) 
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+
+# In[26]:
+
+
+#Fully connect layer
+W_fc1 = weight_variable([7*7*64,1024])
+b_fc1 = bias_variable([1024])
+
+
+# In[28]:
+
+
+h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1)+b_fc1)
+
+
+# In[29]:
+
+
+keep_prob= tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
+
+
+# In[30]:
+
+
+W_fc2 = weight_variable([1024,10])
+b_fc2 = bias_variable([10])
+
+
+# In[35]:
+
+
+y_conv = tf.matmul(h_fc1_drop,W_fc2)+b_fc2
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv,labels=y_))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+
+sess.run(tf.global_variables_initializer())
+
+
+# In[39]:
+
+
+num_steps = 3000
+display = 100
+
+for i in range(num_steps):
+    batch = mnist.train.next_batch(50)
+    train_step.run(feed_dict={x:batch[0],y_:batch[1],keep_prob:0.5})
+    if i%display == 0:
+            train_accuracy = accuracy.eval(feed_dict={                                                     x:batch[0],y_:batch[1],keep_prob:1.0})
+            print("step{0},training accuracy {1:.2f}%".format(i,train_accuracy*100.0))
+print ("Test accuracy {1:.3f}%".format(accuracy.eval(feed_dict={                                                               x:mnist.test.images,                                                                y_:mnist.test.labels,                                                                keep_prob:1.0})*100.0))
+sess.close()
+
